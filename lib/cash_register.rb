b8fc8d7032b746c41ec0d63a6e76b93dd8cbf883
Avery Lin <tclin100@gmail.com>
2021-11-03 10:22:49 -0400

    commit tree 2c6d86ec077e4a75f48e4fdcc31f29aeff7a9aa9

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,34 @@
 
+Class CashRegister
+
+    @@item = []
+
+    def initialize(discount =0)
+            @discount = discount
+            @total = 0
+    end
+
+    def total=(newTotal)
+        @total = newTotal
+    end
+
+    def total
+        @total
+    end
+
+    def discount
+        @discount
+
+    def add_item(title, price, quantity = 1)
+        @total = (price * quantity)
+        item = {title: title, price: price, quantity: quantity}
+        @@items << item
+    end
+
+    def apply_discount
+        discount_amount = @total/@discount
+        @total = @total - discount_amount
+    end
+end
+
+
